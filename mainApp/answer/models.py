d58d8b96eec9a11f555c6f2e1039fbcb0fa16963
Loco <alena.fedorova.12@gmail.com>
2023-09-09 20:39:55 +0200

    change models.py of testresult

--- mainApp/answer/models.py
+++ mainApp/answer/models.py
@@ -7,7 +7,7 @@ class Answer(models.Model):
         YES = 1
 
     question = models.ForeignKey('question.Question', on_delete=models.CASCADE, related_name='questions')
-    testResult = models.ForeignKey('testresult.testResult', on_delete=models.CASCADE, related_name='testresults')
+    test_result = models.ForeignKey('testresult.testResult', on_delete=models.CASCADE, related_name='testresults')
     answer = models.IntegerField(choices=CorrectAnswer.choices)
 
     class Meta:
